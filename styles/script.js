7979e23a3b6bcbf08c0413cab4c455bf8fe09fe4
Hafidz <hafidzfadhillah606@gmail.com>
2021-10-27 20:47:44 +0700

    first commit

---
+++ styles/script.js
@@ -0,0 +1,18 @@
+var countDownDate = new Date("Nov 24, 2021 00:00:00").getTime();
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 *24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("time").innerHTML = days + "h " + hours + "j " + minutes + "m " + seconds + "d ";;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "Happy Anniversary Ke- 2 :)";
+    }
+}, 1000);
